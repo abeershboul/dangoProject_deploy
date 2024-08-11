4833700e7c195836bc8688f37300481266501082
abeershboul <abeer.shboul@yahoo.com>
2024-08-11 15:38:43 +0300

    15

--- build.sh
+++ build.sh
@@ -5,12 +5,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py createsuperuser
 python manage.py migrate
-CMD =["gunicorn", "--bind", "0.0.0.0:8000", "Pet_project.wsgi:application"]
+# CMD =["gunicorn", "--bind", "0.0.0.0:8000", "Pet_project.wsgi:application"]
 # myapp/views.py
-from django.http import HttpResponse
-from django.core.management import call_command
 
-def create_superuser_view(request):
-    if not User.objects.filter(username='admin').exists():
-        call_command('createsuperuser', username='admin', email='admin@example.com', password='password')
-    return HttpResponse("Superuser created")
+exec gunicorn --bind 0.0.0.0:8000 Pet_project.wsgi:application
